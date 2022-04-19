6428d5a832fbed2ce8e9b8844ccfbfc84e3fdf27
R L <lisin.ruslan@gmail.com>
2022-04-19 19:11:58 +0500

    pictures

--- Two Factor Analysis.R
+++ Two Factor Analysis.R
@@ -9,6 +9,8 @@
 # install.packages("Rtools")
 # install.packages("psych")
 # install.packages("Hmisc")
+# install.packages("reshape2")
+
 
 # load libraries
 
@@ -21,6 +23,8 @@ library("FSA")
 library("ggplot2")
 library("psych")
 library("Hmisc")
+library(reshape2)
+library(broom)
 
 
 # work dir
@@ -34,7 +38,7 @@ setwd("e:/RNF_2021-2022/Remodel F-V physiol load-1")
 My_data_table <- read.xlsx("phys_reg.xlsx", sheetIndex = 1)
 
 colnames(My_data_table) <- c("name", "afterload", 'Velocity', 'Work', 'Les', 'temperature', 'camera', 'group')
-
+My_data_table$g_c <- paste(My_data_table$group, My_data_table$camera)   
 # view structure data
 
 str(My_data_table)
@@ -45,41 +49,95 @@ My_data_table$camera <- as.factor(My_data_table$camera)
 My_data_table$group <- as.factor(My_data_table$group)
 My_data_table$name <- as.factor(My_data_table$name)
 My_data_table$afterload <- as.factor(My_data_table$afterload)
+My_data_table$g_c <- as.factor(My_data_table$g_c)
 
 
 # view design - balansed or imbalansed
 
 table(My_data_table$camera, My_data_table$group)
 
-vel <- My_data_table[c("name", "afterload", 'Velocity','camera', 'group')]
+# My_data_table %>% 
+#   group_by(afterload, na.rm=TRUE) %>%
+#   summarise_at(c('Velocity', 'Work', 'Les'), mean, na.rm=TRUE)
 
+# group_df <- group_by(.data = My_data_table, afterload, na.rm=TRUE)
+
+vel <- My_data_table[c("afterload", 'Velocity','camera', 'group',"g_c")]
 vel <- na.omit(vel) 
-# vel <- vel[vel$afterload != 1,]
-vel <- vel[vel$afterload == 0.2,]
-vel <- vel[c('Velocity','camera', 'group')]
+vel <- vel[vel$afterload != 0.1,]
 
-work <- My_data_table[c("name", "afterload", 'Work','camera', 'group')]
-work <- na.omit(Work) 
-Work <- Work[Work$afterload != 0.1,]
-Work <- Work[c('Work','camera', 'group')]
+work <- My_data_table[c( "afterload", 'Work','camera', 'group',"g_c")]
+work <- na.omit(work) 
+work <- work[work$afterload != 0.1,]
+
+les <- My_data_table[c( "afterload", 'Les','camera', 'group',"g_c")]
+les <- na.omit(les) 
+les <- les[les$afterload != 0.1,]
+
+
+# visualisation 
+
+ggplot(vel, aes(x = afterload, y = Velocity, col = camera, shape = group, group = g_c))+
+  # geom_point()+
+  geom_smooth()
+
+
+
+ggplot(vel, aes(x = afterload, y = Velocity, col = camera, shape = group, group = g_c))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'errorbar', width = 0.1, position = position_dodge(0.2))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'point', size = 3, position = position_dodge(0.2))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'line', position = position_dodge(0.2))
+
+ggplot(work, aes(x = afterload, y = Work, col = camera, shape = group, group = g_c))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'errorbar', width = 0.1, position = position_dodge(0.2))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'point', size = 3, position = position_dodge(0.2))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'line', position = position_dodge(0.2))
+
+ggplot(les, aes(x = afterload, y = Les, col = camera, shape = group, group = g_c))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'errorbar', width = 0.1, position = position_dodge(0.2))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'point', size = 3, position = position_dodge(0.2))+
+  stat_summary(fun.data = mean_cl_boot, geom = 'line', position = position_dodge(0.2))
+
 
 # descriptive statistics and normal distribution check
 
-norm_distr <- vel %>%
-  group_by(camera, group, na.rm=TRUE) %>%
+vel <- vel[vel$afterload != 1,]
+work <- work[work$afterload != 1,]
+les <- les[les$afterload != 1,]
+
+
+norm_distr_vel <- vel %>%
+  group_by(camera, group, afterload) %>%
   summarise(n = n(), var = var(Velocity, na.rm=T), sd = sd(Velocity, na.rm=T), method = shapiro.test(Velocity)$method, p.value = shapiro.test(Velocity)$p.value)
+
+norm_distr_work <- work %>%
+  group_by(camera, group, afterload) %>%
+  summarise(n = n(), var = var(Work, na.rm=T), sd = sd(Work, na.rm=T), method = shapiro.test(Work)$method, p.value = shapiro.test(Work)$p.value)
+
+norm_distr_les <- les %>%
+  group_by(camera, group, afterload) %>%
+  summarise(n = n(), var = var(Les, na.rm=T), sd = sd(Les, na.rm=T), method = shapiro.test(Les)$method, p.value = shapiro.test(Les)$p.value)
 
 # check homogeneity of variance 
 # Levene Test
 
-leveneTest(Velocity ~ camera * group, data = vel)
-leveneTest(Velocity ~ group * camera, data = vel)
+leveneTest(Velocity ~ camera * group * afterload, data = vel)
+leveneTest(Work ~ camera * group * afterload, data = work)
+leveneTest(Les ~ camera * group * afterload, data = les)
 
 # Bartlett test
 
 bartlett.test(Velocity ~ camera, data = vel)
 bartlett.test(Velocity ~ group, data = vel)
 bartlett.test(Velocity ~ afterload, data = vel)
+
+bartlett.test(Work ~ camera, data = work)
+bartlett.test(Work ~ group, data = work)
+bartlett.test(Work ~ afterload, data = work)
+
+bartlett.test(Les ~ camera, data = les)
+bartlett.test(Les ~ group, data = les)
+bartlett.test(Les ~ afterload, data = les)
 
 # F test (min group var, max group var)
 
@@ -95,8 +153,15 @@ var.test(filter(vel, camera=="ATRIUM", group=="CONTROL", afterload==0.1)$Velocit
 # Two-way Anova
 # The recommended method are the Type-III sums of squares for unbalanced designs
 
-two_way_anova = aov(Velocity ~ camera * group, data = vel)
+vel_grouped <- vel %>% group_by(afterload)
+two_way_anova = aov(Velocity ~ camera * group + Error(subset=afterload), data = vel)
 "two_way_anova = aov(Data ~ Camera + State + Camera:State, data = My_data_table)"
+
+
+
+datm <- melt(vel, id="afterload", value.name="Percentage", variable.name = "Class")
+res = datm %>% group_by(Species, Class) %>% 
+  do(Model = aov(Percentage ~ TREATMENT, data=.))
 
 
 summary(two_way_anova)
@@ -119,10 +184,26 @@ TukeyHSD(aov(Velocity ~ group, data = filter(vel, camera=="VENTRICLE")))
 TukeyHSD(aov(Velocity ~ group, data = filter(vel, camera=="ATRIUM")))
 # TukeyHSD(aov(Velocity ~ group, data = filter(My_data_table, camera=="S")))
 
+
+
+
 # The Scheirer?Ray?Hare test is a nonparametric test used for a two-way factorial design
 # Note that for unbalanced designs, the scheirerRayHare function uses a type-II sum-of-squares
-vel <- vel[complete.cases(vel),]
+
+
+?scheirerRayHare
 # scheirerRayHare(Velocyty ~ camera * group, data = My_data_table, type=1)
+
+vel_aov <- data.frame(colnames(c('afterload', 'aov')))
+vel_aov <- rbind(vel_aov, c('a','b'))
+vel_aov  
+for(value in unique(vel$afterload)){
+  value <- scheirerRayHare(Velocity ~ camera * group, data = vel[vel$afterload==value,], type = "III")
+  # print(value)
+  view(value)
+  # view(vel[vel$afterload==value,])
+  # scheirerRayHare(Velocity ~ camera * group, data = vel[vel$afterload==value,], type = "III")
+}
 scheirerRayHare(Velocity ~ camera * group, data = vel, type = "III")
 # scheirerRayHare(Velocyty ~ camera * group, data = My_data_table, type=3)
 
@@ -167,3 +248,7 @@ plot(two_way_anova, 1)
 
 
 
+
+
+
+  
